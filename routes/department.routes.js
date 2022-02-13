c70e2e166c0d34cb30a26de26977d6ade19fc4b0
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2022-02-13 04:41:18 +0300

    Update,delete and get routes were added to department routes

--- routes/department.routes.js
+++ routes/department.routes.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const router = express.Router();
-const departmentController = require('../controllers/department.controller')
-router.get('/departments',departmentController.getDepartment);
-router.post('/departments',departmentController.postDepartment);
+const departmentController = require('../controllers/department.controller');
+
+//Get All Departments
+router.get('/departments',departmentController.getDepartments);
+//Get A Department.
+router.get('/departments/:id',departmentController.getDepartment);
+//Post Departments
+router.post('/departments',departmentController.postDepartments);
+//Update Department.
+router.put('/departments/:id',departmentController.updateDepartment);
+//Delete Department.
+router.delete('/departments/:id',departmentController.deleteDepartment);
+
 module.exports = router;
