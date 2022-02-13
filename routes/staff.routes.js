2a65de766352da714634ba8a8464f250c7618b22
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2022-02-13 03:46:43 +0300

    Update,delete routes were added to staff routes

--- routes/staff.routes.js
+++ routes/staff.routes.js
@@ -1,7 +1,16 @@
  const express = require('express');
  const router = express.Router();
  const staffController = require('../controllers/staff.controller');
-
+ 
+ //Get all staff
  router.get('/staffs',staffController.getStaffs);
+// Get one staff
+ router.get('/staffs/:id',staffController.getStaff);
+ //Adding a staff
  router.post('/staffs',staffController.postStaffs);
+ //Updating a staff
+ router.put('/staffs/:id',staffController.updateStaff);
+ //Deleting a staff
+ router.delete('/staffs/:id',staffController.deleteStaff);
+ 
  module.exports = router;
