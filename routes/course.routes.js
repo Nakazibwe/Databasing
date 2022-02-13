ee51ff9db617a9432a0db729cbb438f697ea09df
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2022-02-13 03:48:33 +0300

    Update, delete routes were added to course routes

--- routes/course.routes.js
+++ routes/course.routes.js
@@ -2,6 +2,15 @@ const express = require('express');
 const router = express.Router();
 const courseController = require('../controllers/course.controller');
 
+//Get All Available Courses.
 router.get('/courses',courseController.getCourses);
+//Get One Course
+router.get('/courses/:id',courseController.getCourse)
+//Post Courses
 router.post('/courses',courseController.postCourses);
+//Update Course
+router.put('/courses/:id',courseController.updateCourse);
+//Delete Course
+router.delete('/courses/:id',courseController.deleteCourse);
+
 module.exports = router;
